932747ced933926f85dc3452dbb9e00cbf65e39c
shimon <shimontech116@gmail.com>
2022-07-28 12:48:19 +0900

    add tailwind / google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import '../styles/globals.css'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
       <>
-        <Head>
-          <title>10COUNT IS COMING...</title>
-        </Head>
         <Component {...pageProps} />
       </>
   )
